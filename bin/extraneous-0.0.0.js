7445bfc9ffd55acdfe78e9ed8a11ba6d0f1b478e
Rob <mail@robfors.com>
2018-11-21 00:53:33 -0500

    Shutdown extranEouS when error is thrown when loading a module

--- bin/extraneous-0.0.0.js
+++ bin/extraneous-0.0.0.js
@@ -9,22 +9,42 @@
 
     static load_interpreter(interpreter)
     {
-      interpreter.initialize();
+      if (this._alive == false)
+        throw new Error('can not load interpreter, extranEouS was shutdown');
+      try
+      {
+        interpreter.initialize();
+      }
+      catch (e)
+      {
+        this._alive = false;
+        throw e;
+      }
       this._interpreters.push(interpreter);
-      interpreter.types.forEach(function(type, that){
-        if (Extraneous._source_router.has(type))
+      interpreter.types.forEach(function(type){
+        if (this._source_router.has(type))
           throw new Error("source type already handled");
-        Extraneous._source_router.set(type, interpreter);
-      });
+        this._source_router.set(type, interpreter);
+      }, this);
     }
 
     
     static load_module(module)
     {
-      module.sources.forEach(function(source, that){
-        var interpreter = Extraneous._source_router.get(source.type);
-        interpreter.load(source);
-      });
+      if (this._alive == false)
+        throw new Error('can not load module, extranEouS was shutdown');
+      try
+      {
+        module.sources.forEach(function(source){
+          var interpreter = this._source_router.get(source.type);
+          interpreter.load(source);
+        }, this);
+      }
+      catch (e)
+      {
+        this._alive = false;
+        throw e;
+      }
     };
 
 
@@ -33,6 +53,8 @@
   Extraneous._interpreters = [];
   
   Extraneous._source_router = new Map();
+
+  Extraneous._alive = true;
 
 })();
 Extraneous.Code = class
